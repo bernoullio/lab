ec54bfe6f2b96a1b288f28e3e82827b56743db69
keang <skeang@gmail.com>
2016-08-30 23:19:04 +0800

    Add metadata

--- csv_ingest.py
+++ csv_ingest.py
@@ -3,17 +3,27 @@ Reads csv file and ingests into zipline databundle
 Example:
     - CSV_FILE=fixtures/eur_usd_m1.csv zipline ingest
 """
-
-from pandas import read_csv
+import pandas as pd
 
 def ohlc_from_csv(file_path):
-    df = read_csv(file_path,
+    df = pd.read_csv(file_path,
             header=None,
             names=['date', 'time', 'open', 'high', 'low', 'close', 'volume'],
             parse_dates=[[0, 1]])
     return df.set_index("date_time")
 
-def ingest(environ,
+def metadata(instrument, df):
+    start_date = df.index[0]
+    end_date = df.index[-1]
+    # The auto_close date is the day after the last trade.
+    ac_date = end_date + pd.Timedelta(days=1)
+    return pd.DataFrame({
+        'start_date':      [start_date],
+        'end_date':        [end_date],
+        'auto_close_date': [ac_date],
+        'symbol':          [instrument]})
+
+def csv_ingest(environ,
         asset_db_writer,
         minute_bar_writer,
         daily_bar_writer,
@@ -22,8 +32,10 @@ def ingest(environ,
         cache,
         show_progress,
         output_dir):
-# write metadata
-    #  asset_db_writer.write(metadata(instrument))
-# write minute bars
-    for row in ohlc_from_csv(environ.get("CSV_FILE")):
+
+    df = ohlc_from_csv(environ.get("CSV_FILE"))
+
+    asset_db_writer.write(metadata(instrument, df))
+    for row in df:
         minute_bar_writer(row, show_progress)
+
--- test_csv_ingest.py
+++ test_csv_ingest.py
@@ -4,3 +4,8 @@ def test_ohlc_from_csv():
     df = csv_ingest.ohlc_from_csv('fixtures/eur_usd_m1.csv')
     assert set(df.columns) == set(['open', 'high', 'low', 'close', 'volume'])
 
+def test_metadata():
+    df = csv_ingest.ohlc_from_csv('fixtures/eur_usd_m1.csv')
+    meta = csv_ingest.metadata('EURUSD', df)
+    assert meta.iloc[0]['symbol'] == 'EURUSD'
+
