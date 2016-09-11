46c3f90f9759e45475dbd7f6915a63ffc0cb6d6b
keang <skeang@gmail.com>
2016-09-12 03:03:57 +0800

    Add resampling with specs

---
+++ utils/test_resample.py
@@ -0,0 +1,16 @@
+import os
+import resample
+import pandas as pd
+
+def test_resample_bid_ask():
+    path = 'fixtures/bid-ask.csv'
+    resample.resample_bid_ask(path)
+    new_path = 'fixtures/bid-ask-m1.csv'
+    assert os.path.exists(new_path)
+    df = pd.read_csv(new_path,
+            header=None,
+            names=['name', 'datetime', 'open', 'high', 'low', 'close'],
+            parse_dates=[1])
+    df.set_index("datetime")
+    assert set(df.columns) == set(['datetime', 'open', 'high', 'low', 'close', 'name'])
+
