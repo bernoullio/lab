73c21fb4eece1e53f675219ff2e80cdc4c779163
keang <skeang@gmail.com>
2016-09-13 09:06:32 +0800

    Single responsibility

--- utils/resample.py
+++ utils/resample.py
@@ -12,26 +12,16 @@ def bid_ask_to_ohlc(path):
         EUR/USD,20160729 20:59:56.418,1.11712,1.11781
         EUR/USD,20160729 20:59:56.421,1.11697,1.11781
         EUR/USD,20160729 20:59:56.752,1.11696,1.11799
-    This takes a file like above, resamples into minute ohlc bars,
-    and write to ohlc/<filename>.csv
+    This takes a file like above, resamples into minute ohlc bars
 
     Parameters
     ----------
     path : str
         Path to bid/ask csv data
 
-    Side-effect
-    -----------
-    Writes minute ohlc bars to ohlc/<filename>.csv
-
     Returns
     -------
-    metadata_frame : pd.DataFrame
-        A dataframe with the following columns:
-            start_date     : the first date of data for this asset
-            end_date       : the last date of data for this asset
-            auto_close_date: end_date + one day
-            exchange       : the exchange for the asset; this is always 'forex'
+    ohlc     : the dataframe containing minute bar data
     """
     df = pd.read_csv(path,
             header=None,
@@ -39,23 +29,5 @@ def bid_ask_to_ohlc(path):
             parse_dates=[1])
     df.set_index('datetime', inplace=True)
     df['mid'] = (df['bid']*100000 + df['ask']*100000) // 2
-    ohlc = df.mid.resample('1Min').ohlc()
-
-    dirname, filename = os.path.split(path)
-    new_dir = dirname + "/ohlc/"
-
-    if not os.path.exists(new_dir):
-        os.makedirs(new_dir)
-
-    new_path = new_dir + filename
-    ohlc.to_csv(new_path)
-
-    metadata_frame = pd.DataFrame({
-        "start_date":      [ohlc.index[0]],
-        "end_date":        [ohlc.index[-1]],
-        "auto_close_date": [ohlc.index[-1] + pd.Timedelta(days=1)],
-        "exchange":        ["forex"]
-        })
-
-    return metadata_frame
+    return df.mid.resample('1Min').ohlc()
 
--- utils/test_resample.py
+++ utils/test_resample.py
@@ -6,16 +6,7 @@ import pandas as pd
 
 def test_bid_ask_to_ohlc():
     path = 'fixtures/bid-ask.csv'
-    metadata = resample.bid_ask_to_ohlc(path)
-    new_path = 'fixtures/ohlc/bid-ask.csv'
-    assert os.path.exists(new_path)
-    df = pd.read_csv(new_path,
-            header=1,
-            names=['datetime', 'open', 'high', 'low', 'close'],
-            parse_dates=[0])
-    df.set_index("datetime", inplace=True)
+    df = resample.bid_ask_to_ohlc(path)
     assert set(df.columns) == set(['open', 'high', 'low', 'close'])
-
-    assert set(metadata.columns) == set(['start_date', 'end_date', 'auto_close_date', 'exchange'])
-    assert metadata.start_date[0] == datetime.datetime(2016,7,29,20,50,00)
+    assert df.index[0] == datetime.datetime(2016,7,29,20,50,00)
 
