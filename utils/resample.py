8409d4595c05c0288fc09ceebe345d019faf368d
keang <skeang@gmail.com>
2016-09-13 08:40:16 +0800

    Return metadata

--- utils/resample.py
+++ utils/resample.py
@@ -6,19 +6,32 @@ import pandas as pd
 import pytest
 import os
 
-def resample_bid_ask(path):
+def bid_ask_to_ohlc(path):
     """
     CSV downloaded from truefx comes in the following format:
         EUR/USD,20160729 20:59:56.418,1.11712,1.11781
         EUR/USD,20160729 20:59:56.421,1.11697,1.11781
         EUR/USD,20160729 20:59:56.752,1.11696,1.11799
     This takes a file like above, resamples into minute ohlc bars,
-    and write to <filename>-m1.csv
+    and write to ohlc/<filename>.csv
 
     Parameters
     ----------
     path : str
         Path to bid/ask csv data
+
+    Side-effect
+    -----------
+    Writes minute ohlc bars to ohlc/<filename>.csv
+
+    Returns
+    -------
+    metadata_frame : pd.DataFrame
+        A dataframe with the following columns:
+            start_date     : the first date of data for this asset
+            end_date       : the last date of data for this asset
+            auto_close_date: end_date + one day
+            exchange       : the exchange for the asset; this is always 'forex'
     """
     df = pd.read_csv(path,
             header=None,
@@ -37,5 +50,12 @@ def resample_bid_ask(path):
     new_path = new_dir + filename
     ohlc.to_csv(new_path)
 
-    return new_path
+    metadata_frame = pd.DataFrame({
+        "start_date":      [ohlc.index[0]],
+        "end_date":        [ohlc.index[-1]],
+        "auto_close_date": [ohlc.index[-1] + pd.Timedelta(days=1)],
+        "exchange":        ["forex"]
+        })
+
+    return metadata_frame
 
--- utils/test_resample.py
+++ utils/test_resample.py
@@ -1,11 +1,12 @@
 import os
 import pytest
+import datetime
 import resample
 import pandas as pd
 
-def test_resample_bid_ask():
+def test_bid_ask_to_ohlc():
     path = 'fixtures/bid-ask.csv'
-    resample.resample_bid_ask(path)
+    metadata = resample.bid_ask_to_ohlc(path)
     new_path = 'fixtures/ohlc/bid-ask.csv'
     assert os.path.exists(new_path)
     df = pd.read_csv(new_path,
@@ -14,4 +15,7 @@ def test_resample_bid_ask():
             parse_dates=[0])
     df.set_index("datetime", inplace=True)
     assert set(df.columns) == set(['open', 'high', 'low', 'close'])
+
+    assert set(metadata.columns) == set(['start_date', 'end_date', 'auto_close_date', 'exchange'])
+    assert metadata.start_date[0] == datetime.datetime(2016,7,29,20,50,00)
 
