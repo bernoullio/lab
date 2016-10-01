653fbeda099138fdf3e38880b8180e3cea72a15b
keang <skeang@gmail.com>
2016-10-02 02:49:19 +0800

    Add currencies

--- oanda_ingest.py
+++ oanda_ingest.py
@@ -4,7 +4,21 @@ from toolbox.data import OandaMinutePriceIngest
 
 def ingest(event, context):
     ingest = OandaMinutePriceIngest(os.environ.get("DATABASE_URL"))
-    ingest.run("EUR_USD")
+    currencies = ["EUR_UDS",
+                  "EUR_JPY",
+                  "EUR_GBP",
+                  "EUR_SGD",
+                  "USD_JPY",
+                  "USD_SGD",
+                  "GBP_USD",
+                  "USD_CHF",
+                  "AUD_USD",
+                  "AUD_SGD",
+                  "NZD_USD",
+                  "ZAR_JPY"]
+    for currency in currencies:
+        print("Ingesting {}".format(currency))
+        ingest.run("EUR_USD")
 
 if __name__ == "__main__":
     ingest(None, None)
